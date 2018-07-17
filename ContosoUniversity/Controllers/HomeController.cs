bd1f624264111d1264a582ee07cc69e95a2939a4
Nicholas Biggs <nicholas.biggs@thermofisher.com>
2018-07-17 11:17:08 -0600

    Added about page that tracks a count of the enrollment dates of students at this 'university'

--- ContosoUniversity/Controllers/HomeController.cs
+++ ContosoUniversity/Controllers/HomeController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ContosoUniversity.DAL;
+using ContosoUniversity.ViewModels;
 
 namespace ContosoUniversity.Controllers
 {
     public class HomeController : Controller
     {
+        private SchoolContext db = new SchoolContext();
+
         public ActionResult Index()
         {
             ViewBag.Message = "Welcome to Contoso University";
@@ -17,9 +21,14 @@ namespace ContosoUniversity.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your app description page.";
-
-            return View();
+            var data = from student in db.Students
+                       group student by student.EnrollmentDate into dateGroup
+                       select new EnrollmentDateGroup()
+                       {
+                           EnrollmentDate = dateGroup.Key,
+                           StudentCount = dateGroup.Count()
+                       };
+            return View(data);
         }
 
         public ActionResult Contact()
@@ -27,6 +36,12 @@ namespace ContosoUniversity.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 This project uses ASP.Net MVC 4 to develop an Entity Framework for a fictitional "Contoso University." 
 This is from a tutorial from Microsoft's website, https://docs.microsoft.com/en-us/aspnet/mvc/overview/older-versions/getting-started-with-ef-5-using-mvc-4/ 
 
-*Subjects discussed thus far:*  
+#### *Subjects discussed thus far:*
 - ASP.Net MVC 4 templates  
 - Adding new views and controllers  
 - Implementing Entity Frameworks  
